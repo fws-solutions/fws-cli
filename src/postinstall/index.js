014b8e9a82dd6d3c0db12c8c57df2fcf17b48971
Boris Djemrovski <boris@forwardslashny.com>
2020-06-02 11:11:53 +0200

    Added support for local example.env file

--- src/postinstall/index.js
+++ src/postinstall/index.js
@@ -14,10 +14,12 @@ module.exports = {
         this.createEnv();
     },
 
+    sourceFile: fs.existsSync(path.join(process.cwd(), '/example.env')) ? path.join(process.cwd(), '/example.env') : path.join(__dirname, '/example.env'),
+
     createEnv: function() {
         // check if file already exists
         if (!fs.existsSync(this.directoryFile)) {
-            fs.writeFileSync(this.directoryFile, 'ADMIN_URL=http://admin.some-site.local/', 'utf8');
+            fs.writeFileSync(this.directoryFile, fs.readFileSync(this.sourceFile, 'utf8'), 'utf8');
             helpers.consoleLogWarning('Generated .env file in the root of the Nuxt project! \n    Please configure your local environment.', 'cyan');
         } else {
             helpers.consoleLogWarning('WARNING: .env already exists');
