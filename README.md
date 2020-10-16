0435c287f69810d74accf6f1d13e8a1b77426c77
Nikola Topalović <nikola_topalovic@yahoo.com>
2020-10-16 15:08:42 +0200

    fix github linking

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# README #
+# FORWARDSLASH #
 
 ## @fws/cli
 
--- bin/cli.js
+++ bin/cli.js
@@ -11,7 +11,7 @@ const packageJsonDir = path.join(process.cwd(), '/package.json');
 const packageJson = fs.existsSync(packageJsonDir) ? JSON.parse(fs.readFileSync(packageJsonDir, 'utf8')) : null;
 let w3Command = false;
 
-program.version('0.2.1');
+program.version('0.2.2');
 
 program
     .command('w3-validator <url>')
