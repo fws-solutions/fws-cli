683c97f91cda5cfaeb514dc8a04783d7633d9b90
Nikola Topalović <nikola_topalovic@yahoo.com>
2021-09-09 17:08:23 +0200

    implement checking of components root structure

--- src/file-structure/index.js
+++ src/file-structure/index.js
@@ -1,7 +1,7 @@
 /**
  * File Structure
  *
- * @description CLI script for checking if the file structure in specific project is according to a defined standard.
+ * @description CLI script for checking if the file importedDefinedStructure in specific project is according to a defined standard.
  */
 const fs = require('fs');
 const path = require('path');
@@ -10,7 +10,7 @@ const helpers = require('../helpers');
 const store = require('../store');
 
 const FileStructure = {
-    structure: {
+    importedDefinedStructure: {
         componentsRootDirectory: 'template-views',
         componentsRootStructure: [
             'blocks',
@@ -22,22 +22,74 @@ const FileStructure = {
             '.DS_Store'
         ]
     },
+    definedStructure: [],
+    projectStructure: [],
     componentsRootPath: '',
 
     init: function() {
-        this.componentsRootPath = path.join(store.getters.getProjectRoot(), this.structure.componentsRootDirectory);
+        this.definedStructure = this.importedDefinedStructure.componentsRootStructure;
+        this.componentsRootPath = path.join(store.getters.getProjectRoot(), this.importedDefinedStructure.componentsRootDirectory);
 
-        this.test();
+        this.checkStructure();
     },
 
-    test: function() {
-        var components = fs.readdirSync(this.componentsRootPath);
-
-        var filtered = components.filter((value, index, arr) => {
-            return !this.structure.ignoreFiles.includes(value);
+    checkStructure: function() {
+        const compRootDir = fs.readdirSync(this.componentsRootPath);
+        this.projectStructure = compRootDir.filter(value => {
+            return !this.importedDefinedStructure.ignoreFiles.includes(value);
         });
 
-        console.log(filtered);
+        const isWrongStructureSize = this.checkStructureSizes();
+        const isWrongStructureNames = this.checkStructureNames();
+
+        if (!isWrongStructureSize && !isWrongStructureNames) {
+            console.log(colors.cyan('Components file structure matches. :)'));
+        }
+    },
+
+    checkStructureSizes: function() {
+        // exit if everything matches
+        if (this.projectStructure.length === this.definedStructure.length) {
+            return null;
+        }
+
+        // log lists of directories
+        const projectStructureSize = this.projectStructure.length > this.definedStructure.length ? 'bigger' : 'smaller';
+        let report = colors.yellow(`Project structure is ${colors.red(projectStructureSize)} than defined structure.\n`);
+        report += this.logReportFolderStructure('Defined structure:', this.definedStructure);
+        report += this.logReportFolderStructure('Project structure:', this.projectStructure);
+
+        helpers.consoleLogReport('NUMBER OF DIRs DON\'T MATCH', report);
+
+        return true;
+    },
+
+    checkStructureNames: function() {
+        const mismatched = this.projectStructure.filter(value => {
+            return !this.definedStructure.includes(value);
+        });
+
+        // exit if everything matches
+        if (mismatched.length === 0) {
+            return null;
+        }
+
+        // log mismatched directories
+        let report = colors.yellow(`These directories are ${colors.red('not')} from defined structure:\n`);
+        report += this.logReportFolderStructure('Mismatched directories:', mismatched);
+
+        helpers.consoleLogReport('MISMATCHED STRUCTURE', report);
+
+        return true;
+    },
+
+    logReportFolderStructure: function(title, structureList) {
+        let report = colors.white(`\n\t${title}\n`);
+        report += colors.grey('\t  -');
+        report += colors.grey(structureList.join('\n\t  -'));
+        report += '\n';
+
+        return report;
     }
 };
 
