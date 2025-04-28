170e45a4a64623d5ad8b29b0c85f59e5ec4a64a1
pudja <dejan.pudja@forwardslashny.com>
2025-04-28 12:21:13 +0200

    Fixed npmi/npci and added layout directory for new starter

--- src/commands/npmi.js
+++ src/commands/npmi.js
@@ -1,7 +1,7 @@
 import { getMessageBasedOnCode } from '../util/getMessageBasedOnCode.js';
-import { exec } from 'child_process';
 import { isWin } from '../util/isWin.js';
 import { getPackageMetadata } from '../package/index.js';
+import { spawn } from 'node:child_process';
 
 const npmi = {
     name: 'npmi',
@@ -10,16 +10,22 @@ const npmi = {
     run() {
         const packageMetadata = getPackageMetadata();
         if (!packageMetadata?.isValid) throw new Error(`Script: npm install falied with code: 1`);
+
+        const command = isWin() ? 'npm.cmd' : 'npm';
+        const args = ['install'];
         const config = {
             shell: true,
             cwd: packageMetadata.projectRoot,
+            stdio: 'inherit',
         };
-        const command = isWin() ? 'npm.cmd' : 'npm';
-        const childProcess = exec(command, config);
+
+        const childProcess = spawn(command, args, config);
 
         childProcess.on('close', (code) => {
             console.log(getMessageBasedOnCode(code, 'npm install'));
-            throw new Error(`Script: npm install falied with code: ${code}`);
+            if (code !== 0) {
+                throw new Error(`Script: npm install failed with code: ${code}`);
+            }
         });
     },
 };
